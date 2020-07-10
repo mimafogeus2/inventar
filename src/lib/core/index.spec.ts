a9bbafd56ee63a9f47c4149ecd61b4905ed972bb
Miki Stanger <github@foge.us>
2020-07-10 12:00:59 +0300

    Removed duplicate tests, added new one to core's main file.

--- src/lib/core/index.spec.ts
+++ src/lib/core/index.spec.ts
@@ -3,15 +3,12 @@ import test from 'ava';
 import { resolveConfig, config2CssVars } from '.'
 
 const SIMPLE_CONFIG = { color: '#f00', anotherColor: () => '#0f0' }
-const DEEP_DEPENDENCY_CONFIG = { color: '#0f0', color3: (config) => config.color2, color2: (config) => config.color }
 const SIMPLE_RESOLVED_CONFIG = { primaryColor: '#f00' }
 
-test('processConfig', (t) => {
-  t.deepEqual(resolveConfig(SIMPLE_CONFIG), { color: '#f00', anotherColor: '#0f0' })
-})
-
-test('processConfig deep dependency', (t) => {
-  t.deepEqual(resolveConfig(DEEP_DEPENDENCY_CONFIG), { color:'#0f0', color2: '#0f0', color3: '#0f0' })
+test('resolveConfig', (t) => {
+  const resolvedConfig = resolveConfig(SIMPLE_CONFIG)
+  t.deepEqual(resolvedConfig, { color: '#f00', anotherColor: '#0f0' })
+  t.is(Object.isFrozen(resolvedConfig), true)
 })
 
 test('config2CssVars, default options', (t) => {
