ebe71b1a21fac5d4b24d83134882a05b657f97f4
Miki Stanger <github@foge.us>
2020-08-13 22:52:13 +0300

    Replaced tslint with eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+	parserOptions: {
+		ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+		sourceType: 'module', // Allows for the use of imports
+		ecmaFeatures: {
+			jsx: true, // Allows for the parsing of JSX
+		},
+	},
+	plugins: ['@typescript-eslint', 'prettier'],
+	extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'prettier'],
+	rules: {
+		'@typescript-eslint/explicit-module-boundary-types': 0,
+		'@typescript-eslint/no-explicit-any': 0,
+		'prettier/prettier': 'error',
+	},
+}
